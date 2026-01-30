bb6e5809937264e3f35d14a83722b671643a1f62
dzheng <zzzd3434@gmail.com>
2026-01-30 02:30:59 -0800

    feat(dataset): add DiscreteVLNDataset and its arguments for discrete action prediction

--- joynav/dataset/__init__.py
+++ joynav/dataset/__init__.py
@@ -4,6 +4,8 @@ import joynav.utils.registry
 from joynav.utils.registry import register_component
 from joynav.dataset.streamvln_dataset import StreamVLNDataset
 from joynav.dataset.continuous_action_dataset import ContinuousActionDataset
+from joynav.dataset.discrete_vln_dataset import DiscreteVLNDataset
 
 register_component('dataset', 'streamvln', StreamVLNDataset)
 register_component('dataset', 'continuous_action', ContinuousActionDataset)
+register_component('dataset', 'discrete_vln', DiscreteVLNDataset)
